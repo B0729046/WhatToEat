7be6861a1be38252f676cd4bf3f95747826922b4
B0729046 <B0729046@users.noreply.github.com>
2026-09-10 10:22:42 +0800

    Secure daily finalization endpoint

--- api/finalize.js
+++ api/finalize.js
@@ -1,7 +1,9 @@
-import { finalizePreviousDay } from "./_daily.js";
+import { authorizeCron, finalizePreviousDay } from "./_daily.js";
 export default async function handler(req, res) {
   if (req.method !== "GET")
     return res.status(405).json({ error: "Method not allowed" });
+  if (!authorizeCron(req))
+    return res.status(401).json({ error: "Unauthorized" });
   try {
     return res.status(200).json(await finalizePreviousDay());
   } catch (error) {
